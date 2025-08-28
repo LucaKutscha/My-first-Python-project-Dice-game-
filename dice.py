1f22d8edbeac950e017db42f3ba895bcf82b2d5a
Luca Kutscha <luca_kutscha@web.de>
2025-08-28 16:31:24 +0200

    Beschreibung deiner Änderung

--- dice.py
+++ dice.py
@@ -5,7 +5,7 @@ while True:
     if Choice == "y": 
         die1 = random.randint(1, 6)
         die2 = random.randint(1,6)
-        print(f"{die1}, {die2})")
+        print(f"{die1}, {die2}")
     elif Choice == "n": 
         print("Thanks for playing!")
         break 
